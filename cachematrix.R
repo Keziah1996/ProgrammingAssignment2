0ba3d45084354fecc1ec718a23e6b8ec6241d67f
Keziah1996 <keziah.onditi@strathmore.edu>
2020-07-18 14:12:37 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two functions. One creates a 3 by 3 matrix and assigns its inverse tocache
+## The second function tries to retrieve the value and return it
 
-## Write a short comment describing this function
+## Function 1 below creates a 3 by 3 matrix, calculates its inverse and caches it
 
 makeCacheMatrix <- function(x = matrix()) {
+        x=matrix(c(7,6,5,8,0,9,3,2,1),nrow=3,ncol=3)
+  y<<-x
+    makeCacheMatrix<-function(y)
+    makeCacheMatrix=solve(y)  
+    makeCacheMatrix<<-makeCacheMatrix(y)
+    makeCacheMatrix
 
 }
 
-
-## Write a short comment describing this function
-
+## Function 2 retrieves the cached inverse if the matrix is unchanged and calculates the new inverse if 
+##the value retrieved is null
 cacheSolve <- function(x, ...) {
+        cacheSolve<-function(y)
+    s<-y$makeCacheMatrix
+     if(!is.null(s)){
+      print("Fetching cached data")
+      return(s)
+    } else {
+      data<-y
+      s=solve(y)
+      return(s)
+    }
         ## Return a matrix that is the inverse of 'x'
 }
